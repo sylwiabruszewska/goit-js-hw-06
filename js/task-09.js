e64849a44f3ee696c85c5c74bd22d490214ee521
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-05-29 17:46:01 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorNameSpan = document.querySelector(".color");
+const bodyElement = document.body;
+
+const changeBgColor = () => {
+  const randomColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = randomColor;
+  colorNameSpan.textContent = randomColor;
+};
+
+changeColorBtn.addEventListener("click", changeBgColor);
