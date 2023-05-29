a9cccf4a26d45713e74d8c6f0c6282bc39bb6e14
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-05-29 17:43:24 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const listItems = [];
+ingredients.forEach((element) => {
+  const item = document.createElement("li");
+  item.textContent = element;
+  item.classList.add("item");
+  // list.append(item);
+  listItems.push(item);
+});
+
+list.append(...listItems);
