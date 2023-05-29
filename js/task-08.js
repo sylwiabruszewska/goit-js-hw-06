17154c4d960d46c3a32434920b57530633498352
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-05-29 17:45:05 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
