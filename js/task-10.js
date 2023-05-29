79fb38f9effa098c27cccf8b6f111c712fba51e7
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-05-29 18:22:51 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputField = document.querySelector("#controls input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    boxSize += 10;
+  }
+}
+
+function onCreateBtnClick() {
+  const amount = inputField.value;
+  createBoxes(amount);
+}
+
+function onDestroyBtnClick() {
+  boxesContainer.innerHTML = "";
+}
+
+createBtn.addEventListener("click", onCreateBtnClick);
+destroyBtn.addEventListener("click", onDestroyBtnClick);
